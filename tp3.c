a4b3478e6e9bdcf84f870e3bbcf162f1dae1cd0e
maxgubitosi <mgubitosi@udesa.edu.ar>
2023-05-18 15:15:46 -0300

    v1
    create y destroy funcionan sin errores ni leaks. El put está hecho pero no verificado

--- tp3.c
+++ tp3.c
@@ -1,33 +1,48 @@
 #include "tp3.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
 
+
+#define TABLE_SIZE 7
+// #define RES_FACT 5
+
 // struct para kev-values individuales
 typedef struct dictEntry{
-  char* key;
+  const char* key;
   void* value;
 } dictEntry_t;
 
 // struct para array de punteros dictEntry
 struct dictionary {
   dictEntry_t** entries;
-  int size;
+  uint32_t size;
+  uint32_t capacity;
 };
 
+// funcion auxiliar para imprimir el diccionario
+void print_dict(dictionary_t* dict) {
+  for (uint32_t i = 0; i < dict->size; i++) {
+    printf("key: %s, value: %s\n", dict->entries[i]->key, (char*) dict->entries[i]->value); //problema con el value por ser  void*
+  }
+}
+
 // funcion de hashing FNV-1a
-int *FNV_hash(const char *key) {
-  const uint32_t FNV_OFFSET_BASIS = 2166136261U;
-  const uint32_t FNV_PRIME = 16777619U;
-  const uint8_t *data = (const uint8_t *) key;
+uint32_t FNV_hash(const char *key) {
+  uint32_t FNV_OFFSET_BASIS = 2166136261U;
+  uint32_t FNV_PRIME = 16777619U;
+  uint8_t *data = (uint8_t *) key;
   uint32_t hash = FNV_OFFSET_BASIS;
   size_t len = strlen(key);
   for (size_t i = 0; i < len; i++) {
       hash ^= data[i];
       hash *= FNV_PRIME;
+      // hash %= TABLE_SIZE;  // no hace falta, porque en la función de put() ya se hace
   }
   return hash;
 }
+// mejor no usar los __uint... conviene usar uint_t o size_t
 // deberia probar distintas func de hashing y evaluar cual me da mejores resultados
 
 // creo un diccionario vacio, usando los structs ya definidos y mallocs, verificando que no haya errores
@@ -36,25 +51,30 @@ dictionary_t *dictionary_create(destroy_f destroy) {
   if (dict == NULL) {
     return NULL;
   }
-  dict->entries = (dictEntry_t*) malloc(sizeof(dictEntry_t*));
+  dict->size = 0;
+  dict->capacity = TABLE_SIZE;
+  dict->entries = (dictEntry_t**) calloc(sizeof(dictEntry_t*), dict->capacity);
   if (dict->entries == NULL) {
     return NULL;
   }
-  dict->size = 0;
   return dict;
 }
 
 // inserto un par key-value en el diccionario, verificando que no haya errores
 bool dictionary_put(dictionary_t *dictionary, const char *key, void *value) {
-  int *hash = FNV_hash(key); // % dictionary->size;
+  uint32_t hash = FNV_hash(key) % dictionary->size;
   dictEntry_t* entry = (dictEntry_t*) malloc(sizeof(dictEntry_t));
   if (entry == NULL) {
     return false;
   }
-  dictionary->entries[*hash] = entry;
-  dictionary->entries[*hash]->key = key;
-  dictionary->entries[*hash]->value = value;
+  if (dictionary->entries[hash] != NULL) {
+    return false; // caso a desarrollar: colision
+  }
+  dictionary->entries[hash] = entry;
+  dictionary->entries[hash]->key = key;
+  dictionary->entries[hash]->value = value;
   dictionary->size++;
+  print_dict(dictionary);
   return true;
 }
 // primera versión, no maneja colisiones
@@ -64,9 +84,16 @@ void *dictionary_get(dictionary_t *dictionary, const char *key, bool *err) {
   return NULL;
 };
 
+/* Elimina una clave del diccionario. O(1).
+ * Pre-condiciones
+ * - El diccionario existe
+ * - La clave tiene largo mayor a cero
+ * Retorna true si la clave estaba presente y se pudo eliminar, o false
+ * de otro modo.
+ */
 bool dictionary_delete(dictionary_t *dictionary, const char *key) {
   return true;
-};
+}
 
 void *dictionary_pop(dictionary_t *dictionary, const char *key, bool *err) {
   return NULL;
@@ -78,4 +105,7 @@ bool dictionary_contains(dictionary_t *dictionary, const char *key) {
 
 size_t dictionary_size(dictionary_t *dictionary) { return 0; };
 
-void dictionary_destroy(dictionary_t *dictionary){};
+void dictionary_destroy(dictionary_t *dictionary) {
+  free(dictionary->entries);
+  free(dictionary);
+}
