50458b8502b98d4ed511ce316eeb398acd769a50
maxgubitosi <mgubitosi@udesa.edu.ar>
2023-05-23 15:36:57 -0300

    tests que funcionan con o sin comentar linea

--- tp3.c
+++ tp3.c
@@ -267,7 +267,7 @@ void dictionary_destroy(dictionary_t *dictionary) {
  for (uint32_t i = 0; i < dictionary->capacity; i++) {
     dictEntry_t* entry = dictionary->entries[i];
     if (entry != NULL) {
-      if (entry->value) free(entry->value); //chequear si no es NULL hace que funcionen todos los tests sin leaks
+      if (entry->value) free(entry->value); //con esto comentado funcionan los 3 primeros tests
       free((char *)entry->key); //
       free(entry);
     }
